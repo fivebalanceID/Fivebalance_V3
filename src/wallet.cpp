788bc06439cc7320b38451f8b6015b1eea42b0d8
Fivebalance <43383911+fivebalanceID@users.noreply.github.com>
2019-08-03 09:14:54 +0700

    Update wallet.cpp

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -3654,6 +3654,10 @@ void CWallet::AutoCombineDust()
     for (map<CBitcoinAddress, vector<COutput> >::iterator it = mapCoinsByAddress.begin(); it != mapCoinsByAddress.end(); it++) {
         vector<COutput> vCoins, vRewardCoins;
         vCoins = it->second;
+        
+        // We don't want the tx to be refused for being too large
+        // we use 50 bytes as a base tx size (2 output: 2*34 + overhead: 10 -> 90 to be certain)
+        unsigned int txSizeEstimate = 90;
 
         //find masternode rewards that need to be combined
         CCoinControl* coinControl = new CCoinControl();
@@ -3670,6 +3674,10 @@ void CWallet::AutoCombineDust()
             coinControl->Select(outpt);
             vRewardCoins.push_back(out);
             nTotalRewardsValue += out.Value();
+            // Around 180 bytes per input. We use 190 to be certain
+                        txSizeEstimate += 190;
+                        if (txSizeEstimate >= MAX_STANDARD_TX_SIZE - 200)
+                            break;
         }
 
         //if no inputs found then return
